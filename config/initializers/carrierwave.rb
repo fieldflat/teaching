2a0a7c16979ff56681d53e5faaa6e7ef0c90cb5e
平田智紀 <tomonorihirata@hiratatomonori-no-MacBook-Air.local>
2018-12-23 22:06:25 +0900

    20181223 ver1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,23 @@
-module CarrierWave
-  module MiniMagick
-    def fix_exif_rotation
-      manipulate! do |img|
-        img.auto_orient
-        img = yield(img) if block_given?
-        img
-      end
-    end
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = ENV['AWS_BUCKET_NAME']
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_DEFAULT_REGION'],
+      path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
